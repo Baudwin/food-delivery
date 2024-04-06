c16102044b6b93ee1baba5abd1b2a7820b2f87a9
Titah Baudwin <baudwin12@gmail.com>
2024-04-06 03:00:53 +0100

    navbar styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import { useCartStore } from '../store/useCartStore'
-import { NavLink, Link } from 'react-router-dom'
-import { FaUserAlt } from "react-icons/fa";
+import { NavLink } from 'react-router-dom'
 
 
 export default function Navbar() {
@@ -14,25 +13,39 @@ const display = ()=>{
 }
 
 
-  return (
-    <div className='sticky top-0'>
-    
-        <header className=' bg-amber-50 border-b-2 shadow '>
 
-            <nav className='flex container lg:justify-between md:justify-between sm:justify-between items-center py-4 sm:px-4 md:px-4 '>
+  return (
+    <div className=''>
+    
+        <header className=' bg-black  text-white '>
+
+            <nav className='flex lg:justify-around md:justify-between sm:justify-between items-center py-4 sm:px-4 md:px-4 '>
 
                   <h1 className='text-3xl font-bold text-teal-400 '>FOODIE</h1>
    
                 <div className='flex gap-4 sm:hidden md:hidden'>
-                <NavLink to={'/'} className=' font-bold'>Home</NavLink>
-                 <NavLink to={'menu'} className='  font-bold'>Menu</NavLink>
-                   <NavLink to={"about"} className=' font-bold'>About</NavLink>
-                   <NavLink to={"contact"} className=' font-bold'>Contact</NavLink>
+                <NavLink to={'/'} className='text-lg'>Home</NavLink>
+                 <NavLink to={'menu'} className=' text-lg'>Menu</NavLink>
+                   <NavLink className='text-lg'>About</NavLink>
+                   <NavLink className='text-lg'>Contact</NavLink>
                 </div>
 
 
-        <div className='space-x-2 flex items-center'>
-     <Link to={'profile'}><FaUserAlt className=''/> </Link> 
+        <div className='space-x-3 flex items-center'>
+        <div className=''>
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-person" viewBox="0 0 16 16">
+            <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"/>
+          </svg>
+              </div>
+
+        <div className='relative '>
+            <NavLink to={'/shopping-cart'}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-heart hover:cursor-pointer" viewBox="0 0 16 16">
+              <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15"/>
+            </svg>
+            <span className='absolute bottom-2 left-2.5 w-6 h-6 bg-red-500 text-white font-semibold flex justify-center items-center text-xs rounded-full'>{0}</span>
+            </NavLink>
+         </div>
 
         <div className='relative '>
             <NavLink to={'/shopping-cart'}>
@@ -54,7 +67,7 @@ const display = ()=>{
 
             </nav>
 
-            <div className={`${show ?'flex' : 'hidden'} lg:hidden flex-col justify-center absolute bg-slate-950 w-full `}>
+            <div className={`${show ?'flex' : 'hidden'} lg:hidden flex-col justify-center absolute bg-white w-full `}>
              <NavLink className={`py-4 text-sm px-5 hover:bg-white hover:text-black`}>Home </NavLink>
              <NavLink to={'menu'}  className={`py-4 text-sm px-5 hover:bg-white hover:text-black`}> Menu</NavLink>
              <NavLink  className={`py-4 text-sm px-5 hover:bg-white hover:text-black`}> About </NavLink>
